b5ac71f8240d0d1ae524384d03ba4d79148ca2f6
maplesyrupman <maplesyrupman@protonmail.com>
2021-09-01 20:25:18 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -57,9 +57,15 @@ const fight = (enemyName) => {
 }
 
 for (let i = 0; i < enemyNames.length; i++) {
-    let pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert(`Welcome to Robot Gladiators! Round ${i+1}`);
+        let pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+    } else {
+        window.alert('You have lost your robot in battle! Game Over!');
+        break;
+    }
 }
 
 // fight();
