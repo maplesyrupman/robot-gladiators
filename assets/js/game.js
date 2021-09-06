d4a2a9c7b71cd2260e91b82626ea1eaf3abacb47
maplesyrupman <maplesyrupman@protonmail.com>
2021-09-06 13:01:11 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,10 +7,6 @@ var enemyNames = ['Roborto', 'Amy Android', 'Robo Trumble'];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyNames);
-console.log(enemyNames.length);
-console.log(enemyNames[0]);
-console.log(enemyNames[3]);
 
 // fight function (now with parameter for enemy's name)
 var fight = function (enemyName) {
@@ -27,14 +23,15 @@ var fight = function (enemyName) {
       if (confirmSkip) {
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
         // subtract money from playerMoney for skipping
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney);
         break;
       }
     }
 
     // remove enemy's health by subtracting the amount set in the playerAttack variable
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(
       playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
     );
@@ -53,7 +50,8 @@ var fight = function (enemyName) {
     }
 
     // remove players's health by subtracting the amount set in the enemyAttack variable
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(
       enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
     );
@@ -82,11 +80,11 @@ const startGame = () => {
 
       var pickedEnemyName = enemyNames[i];
 
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
 
       fight(pickedEnemyName);
       if (playerHealth > 0 && i < enemyNames.length - 1) {
-        let storeConfirm = window.confirm('The fight is over, visit the store before the next roung?');
+        let storeConfirm = window.confirm('The fight is over, visit the store before the next round?');
 
         if (storeConfirm) {
           shop();
@@ -159,6 +157,11 @@ const shop = () => {
       shop();
       break;
   }
+}
+
+const randomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min +1)) + min;
+
 }
 
 startGame();
